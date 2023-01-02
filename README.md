3494a0dd00372cbf4be3b9aef19375f38d6b6a73
Mech <83923848+7heMech@users.noreply.github.com>
2023-01-02 15:46:38 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+A really optimized and small express middleware for repl authentication.
+
+## Example
+```js
+const express = require('express');
+const init = require("replit-auth");
+
+const app = express();
+
+const options = {
+	authPage: false, // path to custom auth page, defaults to an optimized version of the replit auth page
+	allRoutes: false // enforce auth on all routes
+};
+
+const auth = init(app, options); // undefined if allRoutes is true
+
+app.get('/', (req, res) => {
+  res.send("Hello Guest!");
+});
+
+// enforce auth on a single route
+app.get('/user', auth, (req, res) => {
+  res.send(`Hello ${req.user.name}!`);
+});
+
+app.listen(3000, () => console.log('Server started!'));
+```
+
+[Live Demo](https://replit.com/@7heMech/ReplAuth?embed=true#index.js)
---
+++ index.js
@@ -0,0 +1,38 @@
+let page;
+
+const parseHeaders = async (req, res, next) => {
+	let user = {};
+	const { headers } = req;
+
+	for (const header in req.user = user, headers) {
+		const value = headers[header];
+
+		if (header.startsWith("x-replit-") && value && "string" == typeof value) {
+			const property = header.replace("x-replit-user-", "").replace(/-(.)/g, (e, t) => t.toUpperCase());
+
+			"roles" === property || "teams" === property ? user[property] = value.split(",") : user[property] = value;
+		}
+	}
+	
+	if (0 === Object.keys(user).length && user.constructor === Object) req.user = null;
+	next();
+}
+
+const auth = (req, res, next) => {
+	if (req.user === null) return res.sendFile(page);
+	next();
+}
+
+/**
+ * @param app Express application instance
+ * @param {Object} options Optional parameters
+ * @param {Boolean} [options.allRoutes] Whether auth should be applied on all routes or not
+ * @param {String} [options.authPage] Path to custom auth page file
+ * @returns {(undefined|function)} undefined if allRoutes = true, otherwise an auth middleware to be used on specific routes
+ */
+module.exports = (app, { allRoutes = false, authPage = __dirname + '/login.html' } = { allRoutes: false, authPage: __dirname + '/login.html' }) => {
+	if (!app) throw "app parameter not defined";
+	page = authPage;
+	app.use(parseHeaders);
+	return allRoutes ? app.use(auth) : auth;
+}
